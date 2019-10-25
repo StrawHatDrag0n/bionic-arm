ae26c9e1a29f3437781f46e99255df4aeed21425
Yash Vaidya <yash.vaidya100@gmail.com>
2019-10-26 00:39:53 +0530

    upload to github

---
+++ control-glove/control-glove.ino
@@ -0,0 +1,65 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+int CE = 7 ;
+int CSN = 8;
+
+int MSG1 = 111; //forward
+int MSG2 = 000; //backward
+int MSG3 = 999;
+int MSG4 = 100; //left
+int MSG5 = 101; //Right
+
+int Forward = A0;
+int  Backward = A1;
+int Right = A2;
+int Left = A3;
+
+RF24 radio(CE, CSN);
+
+
+const uint64_t rxAddr = 1;
+
+void setup()
+{
+  //pinMode(6,OUTPUT);
+  //digitalWrite(6,HIGH);
+  pinMode(Forward,INPUT);
+  pinMode(Backward,INPUT);
+  pinMode(Right,INPUT);
+  pinMode(Left,INPUT);
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(1);
+  radio.stopListening();
+}
+
+void loop()
+{
+  if ( (analogRead(Forward) >= 800) && (analogRead(Forward) <= 900)){
+    radio.write(&MSG1, sizeof(MSG1));
+    Serial.println(MSG1);
+    delay(500);
+  }
+  else if ((analogRead(Backward) >= 800) && (analogRead(Backward) <= 900)){
+    radio.write(&MSG2, sizeof(MSG2));
+    Serial.println(MSG2);
+    delay(500);
+  }
+  else if ( (analogRead(Right) >= 800) && (analogRead(Right) <= 900)){
+    radio.write(&MSG4, sizeof(MSG4));
+    Serial.println(MSG4);
+    delay(500);
+  }
+  else if ((analogRead(Left) >= 800) && (analogRead(Left) <= 900)){
+    radio.write(&MSG5, sizeof(MSG5));
+    Serial.println(MSG5);
+    delay(500);
+  }
+  else{
+    radio.write(&MSG3, sizeof(MSG3));
+    Serial.println(MSG3);
+    delay(500);
+  }
+}
---
+++ robotic-hand/robotic-hand.ino
@@ -0,0 +1,76 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h> 
+
+Servo finger1;
+Servo finger2;
+Servo finger3;
+Servo finger4;
+Servo finger5;
+
+RF24 radio(7,8);
+int pos[5];
+const uint64_t rxAddr ='A';
+int servoPin1 = 22;
+int servoPin2 = 24;
+int servoPin3 = 26;
+int servoPin4 = 28;
+int servoPin5 = 30;
+
+void setup() {
+  // put your setup code here, to run once:
+  finger1.attach(servoPin1);
+  finger2.attach(servoPin2);
+  finger3.attach(servoPin3);
+  finger4.attach(servoPin4);
+  finger5.attach(servoPin5);
+
+  pinMode(servoPin1, OUTPUT);
+  pinMode(servoPin2, OUTPUT);
+  pinMode(servoPin3, OUTPUT);
+  pinMode(servoPin4, OUTPUT);
+  pinMode(servoPin5, OUTPUT);
+
+  finger1.write(30);
+  finger2.write(30);
+  finger3.write(30);
+  finger4.write(30);
+  finger5.write(30);
+  
+  Serial.begin(9600);
+  radio.begin();
+  radio.setRetries(15, 15);
+  radio.openReadingPipe(1, rxAddr);
+  radio.startListening();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+  radio.read(&pos,sizeof(pos));
+  Serial.println(pos[0]);
+  Serial.println(pos[1]);
+  Serial.println(pos[2]);
+  Serial.println(pos[3]);
+  Serial.println(pos[4]);
+
+  if(pos[0] <= 180 && pos[0] > 0)
+  {
+    finger1.write(pos[0]);
+    finger2.write(pos[1]);
+    finger3.write(pos[2]);
+    finger4.write(pos[3]);
+    finger5.write(pos[4]);
+   }
+   else
+   {
+      finger1.write(30);
+      finger2.write(30);
+      finger3.write(30);
+      finger4.write(30);
+      finger5.write(30);
+   }
+   
+  delay(1000);
+}
